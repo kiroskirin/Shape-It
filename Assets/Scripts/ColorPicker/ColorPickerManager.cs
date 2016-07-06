87ab06943fa7deee9b3cac963dadb626586416d0
Sarawut Popadcha <rensamatar@gmail.com>
2016-07-06 16:36:49 +0700

    add something

---
+++ Assets/Scripts/ColorPicker/ColorPickerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorPickerManager : MonoBehaviour {
+
+	public GameObject gameObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GetColor() {
+	}
+}
