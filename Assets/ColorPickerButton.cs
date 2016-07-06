3fe4296213a58fb06083bd6d2be5589475927dd3
Sarawut Popadcha <rensamatar@gmail.com>
2016-07-06 16:18:44 +0700

    implement color picker panel with toggle button

---
+++ Assets/ColorPickerButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorPickerButton : MonoBehaviour {
+
+	public Button colorPickerButton;
+	public GameObject colorPickerPanel;
+
+	private bool isOpen = false;
+
+	void Start () {
+		colorPickerPanel.SetActive (false);
+	}
+	
+	void Update () {
+	
+	}
+
+	public void ToggleColorPanel() {
+		if (isOpen)
+			CloseColorPanel ();
+		else
+			OpenColorPanel ();
+	}
+
+	private void OpenColorPanel() {
+		colorPickerPanel.SetActive (true);
+		isOpen = !isOpen;
+		Time.timeScale = 0;
+	}
+
+	private void CloseColorPanel() {
+		colorPickerPanel.SetActive (false);
+		isOpen = !isOpen;
+		Time.timeScale = 1;
+	}
+}
